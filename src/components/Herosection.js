68c3cc84739a9ccbab0be3db425e49a6faa168c8
LilJho <jhonnelg25@gmail.com>
2023-04-12 13:09:39 +0800

    Update Herosection.js

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -16,7 +16,7 @@ import { MdWavingHand } from "react-icons/md";
 const Herosection = () => {
   return (
     <main className=" w-full min-h-[calc(100vh-86px)] flex flex-col justify-center items-center">
-      <div className="flex justify-center items-center">
+      <div className="flex items-center justify-center">
         <div className="max-w-[20rem] font-montserrat flex flex-col gap-2">
           <div className="flex text-5xl">
             <h1 className="font-semibold">
@@ -33,13 +33,31 @@ const Herosection = () => {
           </p>
           <ul className="flex gap-2">
             <li className="w-8 h-8 flex justify-center items-center rounded-full hover:bg-[#1877F2] hover:text-white">
-              <FaFacebookF />
+              <a
+                href="https://www.facebook.com/lazyJhonnel"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebookF />
+              </a>
             </li>
             <li className="w-8 h-8 flex justify-center items-center rounded-full hover:bg-[#0A66C2] hover:text-white">
-              <FaLinkedinIn />
+              <a
+                href="https://www.linkedin.com/in/jhonnel-garcia-bb0175197/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
             </li>
             <li className="w-8 h-8 flex justify-center items-center rounded-full hover:bg-[#181717] hover:text-white">
-              <FiGithub />
+              <a
+                href="https://github.com/LilJho"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiGithub />
+              </a>
             </li>
           </ul>
         </div>
@@ -49,36 +67,36 @@ const Herosection = () => {
           className="w-[15rem] h-[15rem] rounded-full object-cover"
         />
       </div>
-      <div className="flex justify-center items-center gap-4 absolute bottom-4 text-2xl">
-        <p className="border-r-4 border-black pr-2 font-montserrat font-medium">
+      <div className="absolute flex items-center justify-center gap-4 text-2xl bottom-4">
+        <p className="pr-2 font-medium border-r-4 border-black font-montserrat">
           Tech Stack
         </p>
         <ul className="flex gap-2">
-          <li className="text-[#E34F26] hover:scale-[1.2]">
+          <li className="text-[#E34F26] hover:scale-[1.2]" title="HTML">
             <AiFillHtml5 />
           </li>
-          <li className="text-[#1572B6] hover:scale-[1.2]">
+          <li className="text-[#1572B6] hover:scale-[1.2]" title="CSS">
             <DiCss3 />
           </li>
-          <li className="text-[#F7DF1E] hover:scale-[1.2]">
+          <li className="text-[#F7DF1E] hover:scale-[1.2]" title="JAVASCRIPT">
             <SiJavascript />
           </li>
-          <li className="text-[#61DAFB] hover:scale-[1.2]">
+          <li className="text-[#61DAFB] hover:scale-[1.2]" title="REACTJS">
             <RiReactjsLine />
           </li>
-          <li className="text-[#000] hover:scale-[1.2]">
+          <li className="text-[#000] hover:scale-[1.2]" title="NEXTJS">
             <TbBrandNextjs />
           </li>
-          <li className="text-[#339933] hover:scale-[1.2]">
+          <li className="text-[#339933] hover:scale-[1.2]" title="NODEJS">
             <FaNodeJs />
           </li>
-          <li className="text-[#000] hover:scale-[1.2]">
+          <li className="text-[#000] hover:scale-[1.2]" title="EXPRESSJS">
             <SiExpress />
           </li>
-          <li className="text-[#47A248] hover:scale-[1.2]">
+          <li className="text-[#47A248] hover:scale-[1.2]" title="MONGODB">
             <SiMongodb />
           </li>
-          <li className="text-[#007ACC] hover:scale-[1.2]">
+          <li className="text-[#007ACC] hover:scale-[1.2]" title="TYPESCRIPT">
             <SiTypescript />
           </li>
         </ul>
