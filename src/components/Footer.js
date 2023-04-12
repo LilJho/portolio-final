ef6a5a16090dd8e3ebd5ae81d389e99cdbc8dec1
LilJho <jhonnelg25@gmail.com>
2023-04-12 13:09:53 +0800

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-[#242424] p-4 flex justify-center items-center">
+      <h1 className="text-xs text-white font-poppins">
+        © 2023 All rights Reserved - Made by Jhonnel Garcia
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
