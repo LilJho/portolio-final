d1c98767edee76ba9bdf5e98e49851994096c301
LilJho <jhonnelg25@gmail.com>
2023-04-12 13:08:36 +0800

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,37 @@
 import logo from "../assets/logo.svg";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center w-full px-4 py-2">
+    <nav className="flex items-center justify-between w-full px-4 py-2">
       <div className="cursor-pointer">
         <img src={logo} alt="Logo of the web app" />
       </div>
-      <ul className="flex gap-4 font-montserrat font-medium">
-        <li className="cursor-pointer">About me</li>
-        <li className="cursor-pointer">Projects</li>
+      <ul className="flex gap-4 text-xs font-semibold font-montserrat">
+        <li className="transition-colors cursor-pointer hover:text-blue-600">
+          <Link
+            activeClass="active"
+            to="about-section"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            About me
+          </Link>
+        </li>
+        <li className="transition-colors cursor-pointer hover:text-blue-600">
+          <Link
+            activeClass="active"
+            to="projects-section"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
         <li className="cursor-pointer">Contact</li>
       </ul>
     </nav>
